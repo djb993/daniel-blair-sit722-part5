80d3bbbf497a425dc7352f6c67a5bba709435263
Dan Blair <s223215521@deakin.edu.au>
2024-07-29 12:53:29 +1000

    Created disply-status-launch-browser.sh to display active pods, services and deployments in terminal. Retrieve External IPs when loaded, format URLs and launch both microservices in browser

---
+++ scripts/display-status-launch-browser.sh
@@ -0,0 +1,46 @@
+# Terminal text output colour variables: purple & default
+p='\033[0;35m'
+d='\033[0m'
+
+
+echo -e "\n${p}Waiting 30 seconds for services to deploy...${d}\n"
+sleep 30
+
+echo "----------------------------------- Active Pods -----------------------------------"
+echo "-----------------------------------------------------------------------------------"
+kubectl get pods
+echo -e "-----------------------------------------------------------------------------------\n"
+
+echo "------------------------------------- Active Services ----------------------------------------- "
+echo "-----------------------------------------------------------------------------------------------" 
+kubectl get services
+echo -e "-----------------------------------------------------------------------------------------------\n"
+
+echo "---------------------------- Active Deployments ----------------------"
+echo "----------------------------------------------------------------------"
+kubectl get deployments
+echo -e "----------------------------------------------------------------------\n"
+
+echo -e "${p}Waiting 30 seconds for External IPs to initiate...${d}\n"
+sleep 30
+
+# Get External IP addresses
+BOOK_CATALOG_IP=$(kubectl get service book-catalog-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+INVENTORY_MANAGEMENT_IP=$(kubectl get service inventory-management-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# Get External Ports
+BOOK_CATALOG_PORT=$(kubectl get service book-catalog-service -o jsonpath='{.spec.ports[0].port}')
+INVENTORY_MANAGEMENT_PORT=$(kubectl get service inventory-management-service -o jsonpath='{.spec.ports[0].port}')
+# Define Service URLs
+BOOK_CATALOG_URL="http://$BOOK_CATALOG_IP:$BOOK_CATALOG_PORT/docs"
+INVENTORY_MANAGEMENT_URL="http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/docs"
+
+echo "---------------------------------- Service URLS -----------------------------------"
+echo "-----------------------------------------------------------------------------------"
+echo BOOK CATALOG URL = "http://$BOOK_CATALOG_IP:$BOOK_CATALOG_PORT/docs"
+echo INVENTORY MANAGEMENT URL = "http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/docs"
+echo -e "-----------------------------------------------------------------------------------\n"
+
+# Launch Browser Showing Services
+echo -e "${p}Opening Services in Browser Shortly...${d}"
+sleep 30
+start "" "chrome.exe" --new-window "$BOOK_CATALOG_URL" "$INVENTORY_MANAGEMENT_URL"
