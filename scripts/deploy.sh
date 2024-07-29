99472b9fcd796f57c39d9fc5649b1654c0a5840f
Dan Blair <s223215521@deakin.edu.au>
2024-07-29 13:29:20 +1000

    Updated deploy.sh to run scripts in sequence and launch the microservices from scratch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,22 @@
 p='\033[0;35m'
 d='\033[0m'
 
+
+# Provision Infrastructure
+./scripts/provision-infrastructure.sh
+
+# Build Images 
+./scripts/build-image.sh
+
+# Push Images
+./scripts/push-image.sh
+
+# Configure Infrastructure
+./scripts/configure-infrastructure.sh
+
 # Deploy Microservices
 echo -e "\n${p}Deploying Microservices...${d}\n" 
 kubectl apply -f ./scripts/kubernetes/deployment.yaml
+
+# Display Status & Launch Browser
+./scripts/display-status-launch-browser.sh
