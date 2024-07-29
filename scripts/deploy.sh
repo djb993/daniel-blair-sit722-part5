c29800d9399ab3f57f1ce44ad661139de27a7d07
Dan Blair <s223215521@deakin.edu.au>
2024-07-29 12:42:39 +1000

    Created deploy.sh file to deploy microscervices

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,8 @@
-# Your Solution
+### Launch file using .scripts/deploy.sh (in Git Bash) ###
+# print colour
+p='\033[0;35m'
+d='\033[0m'
+
+# Deploy Microservices
+echo -e "\n${p}Deploying Microservices...${d}\n" 
+kubectl apply -f ./scripts/kubernetes/deployment.yaml
