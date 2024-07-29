28f1287c2076ef1a681045e4b83a037308402def
Dan Blair <s223215521@deakin.edu.au>
2024-07-29 13:43:13 +1000

    Created delete.sh to delete the microservice Deployments and Services, as well as the provisioned Azure Resources and Images

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,25 @@
-# Your Solution
+### Launch file using .scripts/deploy.sh (in Git Bash) ###
+# print colour
+p='\033[0;35m'
+d='\033[0m'
+
+echo -e "\n${p}Delete Kubernetes Deployments annd Services${d}\n"
+kubectl delete -f scripts/kubernetes/deployment.yaml
+
+echo -e "\n${p}Assigning Owner Permission To Delete Resources...${d}\n"
+az role assignment create --assignee odl_user_1402445@cloudlabs4deakin.onmicrosoft.com --role "Owner" --scope subscriptions/a9812c10-79b1-4c84-a39f-4e5793746024/resourceGroups/djb993rg
+
+echo -e "\n${p}Refreshing to Update Permission Status${d}\n"
+az logout
+az login
+
+echo -e "\n${p}Navigate to Terraform Directory${d}\n"
+cd ./terraform
+pwd
+
+echo -e "\n${p}Deleting Azure Platform Resources...${d}\n"
+terraform destroy -auto-approve
+
+echo -e "\n${p}Removing Images...${d}\n"
+docker rmi djb993cr.azurecr.io/book-catalog-service:1
+docker rmi djb993cr.azurecr.io/inventory-management-service:1
