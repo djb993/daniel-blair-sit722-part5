0f8bd7f199af98c033c13ef021dd4139f45bf352
Dan Blair <s223215521@deakin.edu.au>
2024-07-30 16:53:00 +1000

    Test Workflow 11

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,15 +8,18 @@
 p='\033[0;35m'
 d='\033[0m'
 
-set -u # or set -o nounset
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
+set -euo pipefail
 
 echo -e "\n${p}Deploying Microservices...${d}\n"
 
+# Ensure KUBECONFIG is set
+export KUBECONFIG="$HOME/secrets/kubeconfig.txt"
+
 echo "KUBECONFIG in deploy.sh: $KUBECONFIG"
 
-envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f -
+envsubst < ./scripts/kubernetes/deploy.yaml | kubectl apply -f -
+
+echo -e "\n${p}Deployment complete.${d}\n"
 
 # Display Status & Launch Browser
 ./scripts/display-status-launch-browser.sh
