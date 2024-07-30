06309312a3fcc7ac9ea1c9fe085c7d42a45df2f7
Dan Blair <s223215521@deakin.edu.au>
2024-07-30 15:47:01 +1000

    Test Workflow 6

--- scripts/decrypt-secret.sh
+++ scripts/decrypt-secret.sh
@@ -2,9 +2,7 @@
 
 # Decrypt the file
 mkdir -p $HOME/secrets
-
-echo "decrypt-secret.sh has run"
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/kubekubeconfig.txt kubeconfig.txt.gpg
+--output - kubeconfig.txt.gpg | base64 -d > $HOME/secrets/kubeconfig.txt
